a84de6f0005fc507863b1b0cacd781c2aebb8fec
AUDREY KADJAR <audreykadjar@gmail.com>
2020-08-23 20:10:42 +0200

    built house with styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Bakery from "./Bakery.js";
 import "./App.scss";
-import axios from "axios";
 
 function App() {
   return (
-    <div>
-      <div></div>
+    <div className="App d-flex align-items-center justify-content-center">
+      <Bakery> </Bakery>
     </div>
   );
 }
--- client/src/Bakery.js
+++ client/src/Bakery.js
@@ -3,9 +3,30 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Bakery() {
   return (
-    <div>
-      <div> </div>
-    </div>
+    <Container fluid>
+      <Row className="bakery-drawing d-flex flex-column align-items-center">
+        <div className="sun"> </div>
+        <div className="roof"></div>
+
+        <div className="body d-flex flex-column align-items-center">
+          <div className="sign d-flex justify-content-center align-items-center">
+            <span className="sign-writing"> COOKIE BAKERY </span>
+          </div>
+          <div className="windows d-flex justify-content-between">
+            <div className="window window1"></div>
+            <div className="window window2"></div>
+          </div>
+          <div className="door d-flex align-items-center">
+            <div className="door-handle"></div>
+          </div>
+
+          <div className="mailbox d-flex flex-column align-items-center">
+            <div className="mailbox-opening"> </div>
+            <span className="mailbox-title"> Mailbox </span>
+          </div>
+        </div>
+      </Row>
+    </Container>
   );
 }
 
